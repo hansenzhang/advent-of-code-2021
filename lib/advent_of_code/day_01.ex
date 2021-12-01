5f7f37736fab81b3256c73221389a7d8e98de17c
Hansen Zhang <hansen@fulcrumpro.com>
2021-12-01 10:41:08 -0500

    Adding solutions for day 1

--- lib/advent_of_code/day_01.ex
+++ lib/advent_of_code/day_01.ex
@@ -1,7 +1,55 @@
-defmodule AdventOfCode.Day01 do
-  def part1(args) do
+defmodule Mth do
+
+  def sum_list([]) do
+    0
   end
 
+  def sum_list([h|t]) do
+    h + sum_list(t)
+  end
+
+end
+
+defmodule AdventOfCode.Day01 do
+  def part1(args) do
+    list = String.split(args) |> Enum.map(&String.to_integer/1)
+    [head | tail] = list
+    isGreater(head, tail)
+  end
+
+  def isGreater(prev, [head | tail]) do
+    if head > prev do
+      1 + isGreater(head, tail)
+    else
+      isGreater(head, tail)
+    end
+  end
+
+  def isGreater(_, []) do
+    0
+  end
+
+
   def part2(args) do
+    list = String.split(args)
+        |> Enum.map(&String.to_integer/1)
+        |> Enum.chunk_every(3, 1, :discard)
+        |> Enum.map(fn x -> Enum.sum(x) end)
+    [head | tail] = list
+    isGreater(head, tail)
+
+    #isGreaterChunk(head, tail)
+  end
+
+  def isGreaterChunk(prev, [head | tail]) do
+    if  Mth.sum_list(head) >  Mth.sum_list(prev) do
+      1 + isGreaterChunk(head, tail)
+    else
+      isGreaterChunk(head, tail)
+    end
+  end
+
+  def isGreaterChunk(_, []) do
+    0
   end
 end
--- test/advent_of_code/day_01_test.exs
+++ test/advent_of_code/day_01_test.exs
@@ -3,19 +3,19 @@ defmodule AdventOfCode.Day01Test do
 
   import AdventOfCode.Day01
 
-  @tag :skip
+  #@tag :skip
   test "part1" do
-    input = nil
+    input = AdventOfCode.Input.get!(1, 2021)
     result = part1(input)
 
-    assert result
+    assert result == 1564
   end
 
-  @tag :skip
+  #@tag :skip
   test "part2" do
-    input = nil
+    input = AdventOfCode.Input.get!(1, 2021)
     result = part2(input)
 
-    assert result
+    assert result == 1611
   end
 end
