a06c5613448439cee2511ca79b0b1a1ec9a59e29
Hansen Zhang <hansen@fulcrumpro.com>
2021-12-02 08:49:49 -0500

    simplify day02 to use Enum.reduce

--- lib/advent_of_code/day_02.ex
+++ lib/advent_of_code/day_02.ex
@@ -4,21 +4,16 @@ defmodule AdventOfCode.Day02 do
     |> String.trim()
     |> String.split("\n")
     |> Enum.map(fn x -> String.split(x, " ") |> List.to_tuple() end)
-    |> reduce_to_commands(%{"forward" => 0, "down" => 0, "up" => 0})
+    |> Enum.reduce(%{"forward" => 0, "down" => 0, "up" => 0},
+      fn (head, acc) ->
+        command = elem(head, 0)
+        value = String.to_integer(elem(head, 1))
+        prevValue = acc[command]
+        Map.put(acc, command, value + prevValue)
+      end
+    )
 
     result["forward"] * (result["down"] - result["up"])
-  end
-
-  @spec reduce_to_commands(nonempty_maybe_improper_list, map) :: any
-  def reduce_to_commands([head | tail], commands) do
-    command = elem(head, 0)
-    value = String.to_integer(elem(head, 1))
-    prevValue = commands[command]
-    reduce_to_commands(tail, Map.put(commands, command, value + prevValue))
-  end
-
-  def reduce_to_commands([], commands) do
-    commands
   end
 
   def part2(args) do
@@ -26,25 +21,20 @@ defmodule AdventOfCode.Day02 do
     |> String.trim()
     |> String.split("\n")
     |> Enum.map(fn x -> String.split(x, " ") |> List.to_tuple() end)
-    |> reduce_aim_commands(%{"forward" => 0, "down" => 0, "up" => 0, "aim" => 0})
+    |> Enum.reduce(%{"forward" => 0, "down" => 0, "up" => 0, "aim" => 0},
+      fn (head, acc) ->
+        command = elem(head, 0)
+        value = String.to_integer(elem(head, 1))
+        aim = acc["aim"]
+        prevValue = acc[command]
+        depth = acc["down"]
+        case command do
+          "down"    -> Map.put(acc, "aim", value + aim)
+          "up"      -> Map.put(acc, "aim", aim - value)
+          "forward" -> %{acc | "down" => aim * value + depth , "forward" => value + prevValue}
+        end
+      end)
 
     result["forward"] * result["down"]
-  end
-
-  def reduce_aim_commands([head | tail], commands) do
-    command = elem(head, 0)
-    value = String.to_integer(elem(head, 1))
-    aim = commands["aim"]
-    prevValue = commands[command]
-    depth = commands["down"]
-    case command do
-      "down"    -> reduce_aim_commands(tail, Map.put(commands, "aim", value + aim))
-      "up"      -> reduce_aim_commands(tail, Map.put(commands, "aim", aim - value))
-      "forward" -> reduce_aim_commands(tail, %{commands | "down" => aim * value + depth , "forward" => value + prevValue})
-    end
-  end
-
-  def reduce_aim_commands([], commands) do
-    commands
   end
 end
