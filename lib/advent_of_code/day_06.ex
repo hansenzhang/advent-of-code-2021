701972fd48eaed9faa87c2724bcdf7f50de186b6
Hansen Zhang <hansen@fulcrumpro.com>
2021-12-06 09:28:55 -0500

    Adding day 6 solutions

--- lib/advent_of_code/day_06.ex
+++ lib/advent_of_code/day_06.ex
@@ -1,7 +1,76 @@
 defmodule AdventOfCode.Day06 do
   def part1(args) do
+    fishies =
+      args
+      |> String.trim()
+      |> String.split(",")
+      |> Enum.map(&String.to_integer/1)
+
+    IO.inspect(fishies)
+
+    days =
+      for x <- 1..80,
+          do: x
+
+    days
+    |> Enum.reduce(fishies, &iterate/2)
+    |> Enum.count()
   end
 
   def part2(args) do
+    # "3,4,3,1,2"
+    fishies =
+      args
+      |> String.trim()
+      |> String.split(",")
+      |> Enum.map(&String.to_integer/1)
+      |> Enum.frequencies()
+
+    empty = %{0 => 0, 1 => 0, 2 => 0, 3 => 0, 4 => 0, 5 => 0, 6 => 0, 7 => 0, 8 => 0}
+
+    fishies = Map.merge(empty, fishies)
+
+    days =
+      for x <- 1..256,
+          do: x
+
+    days
+    |> Enum.reduce(fishies, &iterate_better/2)
+    |> Map.values()
+    |> Enum.sum()
+  end
+
+  defp iterate(_day, fishies) do
+    new_fishies =
+      fishies
+      |> Enum.filter(&(&1 === 0))
+      |> Enum.map(fn _n -> 8 end)
+
+    fishies =
+      fishies
+      |> Enum.map(fn fish ->
+        if fish === 0 do
+          6
+        else
+          fish - 1
+        end
+      end)
+
+    new_fishies ++ fishies
+  end
+
+  defp iterate_better(_day, fishies) do
+    %{
+      fishies
+      | 0 => fishies[1],
+        1 => fishies[2],
+        2 => fishies[3],
+        3 => fishies[4],
+        4 => fishies[5],
+        5 => fishies[6],
+        6 => fishies[7] + fishies[0],
+        7 => fishies[8],
+        8 => fishies[0]
+    }
   end
 end
